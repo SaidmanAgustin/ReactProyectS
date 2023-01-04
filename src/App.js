e45494e9c955820d8df38fc03e3a049a911c1d30
SaidmanAgustin <agustinsaidman@gmail.com>
2023-01-04 20:31:37 -0300

    AgregandoComponentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
+import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Router, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
-import Blog from './components/paginas/Blog';
-import Inicio from './components/paginas/Inicio';
-import Viajes from './components/paginas/Viajes';
+//import NavBar from './components/NavBar/NavBar';
+//import Blog from './components/paginas/Blog';
+//import Inicio from './components/paginas/Inicio';
+//import Viajes from './components/paginas/Viajes';
 
 
 function App() {
   return (
- <div className= 'App'>
-  <Router>
-
-    <NavBar />
-    <Switch>
-      <Route path='/Inicio' exact component={Inicio}/>
-      <Route path='/blog'  component={Blog}/>
-      <Route path='/Viajes' component={Viajes}/>
-    </Switch>
-
-  </Router>
- </div>
+    <><NavBar /></>
 
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import styled from "styled-components";
 
-export const NavBar =() => {
-  return (
- 
- <div>
-  <ul className="nav justify-content-center">
-  <li className="nav-item">
-    <Link className="nav-link" to='/'>Inicio</Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link" to='/Viajes'>Viajes</Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link disabled" to='Blog'>Blog</Link>
-  </li>
-</ul>
- </div>
-  )
+
+function NavBar() {
+  <>
+    <NavContainer>
+      <h2>NavBar</h2>
+    </NavContainer>
+  </>
 }
+
 export default NavBar;
 
+const NavContainer = styled.nav;
